5b7899762ab008eaa7198bb6cfc45fe786e606e6
TehvanM <Tehvan.marjapuu@gmail.com>
2025-02-11 14:43:34 +0200

    tr5

--- PHP/php.php
+++ PHP/php.php
@@ -3,22 +3,26 @@
 /**
  * PHP
     01 - PHP - Muutujad
-	Tehvan Marjapuu
-	Haapsalu Kutsehariduskeskus
+    Tehvan Marjapuu
+    Haapsalu Kutsehariduskeskus
     11.02.2025
-    
  */
-
 
 $nimi = "Tanel";
 $sunniaasta = "2015";
 $tahtkuju = "Kaksikud";
 
-echo "Nimi: $nimi\nSünniaasta: $sunniaasta\nTähtkuju: $tahtkuju\n";
+echo "Nimi: $nimi" . '<br>';
+echo "Sünniaasta: $sunniaasta" . '<br>';
+echo "Tähtkuju: $tahtkuju" . '<br>';
 
 // Väljasta järgnev lause
-echo "\"It’s My Life\" – Dr. Alban\n";
+echo "\"It’s My Life\" – Dr. Alban" . '<br>';
 
 // "Joonista" järgmine pilt
-echo "(\(\  \n ( -.-) \n o_(\")(\")\n *** \n  *  \n";
+echo "(\(\\" . '<br>';
+echo "( -.-)" . '<br>';
+echo "o_(\")(\")" . '<br>';
+
+
 ?>
