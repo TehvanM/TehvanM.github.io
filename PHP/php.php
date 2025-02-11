f5deac7c837b889b264059259f24274204173394
TehvanM <Tehvan.marjapuu@gmail.com>
2025-02-11 13:32:35 +0200

    t

---
+++ PHP/php.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PHP
+    01 - PHP - Muutujad
+	Tehvan Marjapuu
+	Haapsalu Kutsehariduskeskus
+    11.02.2025
+    
+ */
+
+
+$nimi = "Tanel";
+$sunniaasta = "2015";
+$tahtkuju = "Kaksikud";
+
+echo "Nimi: $nimi\nSünniaasta: $sunniaasta\nTähtkuju: $tahtkuju\n";
+
+// Väljasta järgnev lause
+echo "\"It’s My Life\" – Dr. Alban\n";
+
+// "Joonista" järgmine pilt
+echo "(\(\  \n ( -.-) \n o_(\")(\")\n *** \n  *  \n";
+?>
